150bd90ff5bdab516a1f86b0b05f5de28fe32930
nuriavt1 <134273029+nuriavt1@users.noreply.github.com>
2026-02-25 22:47:26 +0100

    header icons bigger

--- nuriaportfoliosite/src/components/header.js
+++ nuriaportfoliosite/src/components/header.js
@@ -45,10 +45,10 @@ const navigation = [
             <button
               type="button"
               onClick={() => setMobileMenuOpen(true)}
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-[#270400]"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-3 text-[#270400]"
               aria-label="Open menu"
             >
-              <Bars3Icon aria-hidden="true" className="size-6" />
+              <Bars3Icon aria-hidden="true" className="size-8" />
             </button>
           </div>
 
@@ -95,16 +95,16 @@ const navigation = [
                   <DialogPanel className="w-full overflow-y-auto bg-[#E63A27] p-6 shadow-2xl sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
                       <Link to="/" className="-m-1.5 p-1.5" onClick={() => setMobileMenuOpen(false)}>
-                        <img alt="Logo" src="/imatges/white_logo.png" className="h-8 w-auto" />
+                        <img alt="Logo" src="/imatges/white_logo.png" className="h-10 w-auto" />
                       </Link>
 
                       <button
                         type="button"
                         onClick={() => setMobileMenuOpen(false)}
-                        className="-m-2.5 rounded-md p-2.5 text-white transition-transform duration-200 hover:scale-105"
+                        className="-m-2.5 rounded-md p-3 text-white transition-transform duration-200 hover:scale-105"
                         aria-label="Close menu"
                       >
-                        <XMarkIcon aria-hidden="true" className="size-6" />
+                        <XMarkIcon aria-hidden="true" className="size-8" />
                       </button>
                     </div>
 
